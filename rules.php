c34a78b0f7bc594258bdc28c7ffb0cbd82ddd874
Astatroth <astatroth@gmail.com>
2024-03-15 17:04:03 +0500

    Updated skeleton

--- rules.php
+++ rules.php
@@ -6,4 +6,9 @@ return [
     'config/mypackage.php' => 'config/:lc:package.php',
     'src/Facades/MyPackage.php' => 'src/Facades/:uc:package.php',
     'src/MyPackageServiceProvider.php' => 'src/:uc:packageServiceProvider.php',
+    'resources/css/mypackage.scss' => 'resources/css/:lc:.scss',
+    'resources/js/mypackage.js' => 'resources/js/:lc:.js',
+    'resources/lang/en/mypackage.php' => 'resources/lang/en/:lc:.php',
+    'resources/lang/ru/mypackage.php' => 'resources/lang/ru/:lc:.php',
+    'resources/lang/uz/mypackage.php' => 'resources/lang/uz/:lc:.php',
 ];
